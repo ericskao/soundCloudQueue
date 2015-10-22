2dcd14cceb0992dd7eaf25ef98955cf59bf76576
Eric Kao <ericskao@gmail.com>
2015-10-21 17:50:47 -0700

    set up lobby controller, working on sendMsg

--- public/js/controllers/LobbyCtrl.js
+++ public/js/controllers/LobbyCtrl.js
@@ -1,5 +1,11 @@
-angular.module('LobbyCtrl', []).controller('LobbyController', function($scope) {
-
+angular.module('LobbyCtrl', []).controller('LobbyController', function($scope, $log) {
 	$scope.tagline = 'LOBBY!';
+  $scope.nickName = "Guest" + Math.floor(Math.random()*100);
+  $scope.messageLog = 'Ready to chat!';
+  $scope.sendMessage = function(){
+    console.log('foobar');
+    $scope.message = '';
+    console.log($scope.nickName);
+  };
 
 });
